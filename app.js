24607650e685c2aeccd10241cbbe77e013949f27
Luis Paredes <lualps94@gmail.com>
2019-02-13 12:47:56 -0400

    Set up basic chat

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+
+  socket.on('messagesent', (data) => {
+    console.log(`A message was sent: ${data}`);
+  });
 });
 
 http.listen(3000, () => {
