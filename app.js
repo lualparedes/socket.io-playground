ade0415db609d9032133953aacca09eb4acab731
Luis Paredes <lualps94@gmail.com>
2019-02-20 12:49:29 -0400

    Added basic room support

--- app.js
+++ app.js
@@ -6,17 +6,47 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+
+
 io.on('connection', (socket) => {
+
   console.log('A user connected');
+
+
+  // Room management
+  const numOfConnectedClients = Object.keys(io.of('/').sockets).length;
+
+  if (numOfConnectedClients <= 2) {
+    console.log(`someone joined, now we're ${numOfConnectedClients}`);
+    socket.join('my-awesome-default-room', () => {
+      console.log(`${numOfConnectedClients} in my-awesome-default-room`);
+    });
+  }
+  else {
+    console.log(`TOO MANY PEOPLE!, now we're ${numOfConnectedClients}`);
+    socket.join('rejection-room', () => {
+      console.log(`User couldn't get into my-awesome-default-room`);
+    });
+  }
+
+  io.of('/').in('my-awesome-default-room').emit('accept');
+  io.of('/').in('rejection-room').emit('reject');
+
+
+  // Regular event management
+  socket.on('messagesent', (data) => {
+    console.log(`A message was sent: ${data.message}`);
+    if (data.room == 'my-awesome-default-room') {
+      io.of('/').in(data.room).emit('newmessage', data.message);
+    }
+  });
 
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
-
-  socket.on('messagesent', (data) => {
-    console.log(`A message was sent: ${data}`);
-  });
 });
+
+
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
